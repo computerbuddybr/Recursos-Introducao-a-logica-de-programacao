87eddd0d1265b5adbc6bf25b4cc533f3ceeca6d6
Adriana Cerdeira - Computer Buddy BR <90341758+computerbuddybr@users.noreply.github.com>
2022-02-02 17:20:26 -0300

    Add files via upload

--- RecursosJS/Aula_09_Encapsulamento_Funcoes.js
+++ RecursosJS/Aula_09_Encapsulamento_Funcoes.js
@@ -1,7 +1,11 @@
 //Função com retorno
 function minhaPrimeiraFuncao(primeiroParametro, segundoParametro){
+    if(primeiroParametro == 4){
+        return "Oopsie";
+    }
 
-    return primeiroParametro + segundoParametro;
+    return primeiroParametro * segundoParametro;
+    console.log("depois do return");
 }
 
 //Função sem retorno
@@ -13,12 +17,19 @@ function minhaSegundaFuncao(a,b){
 
 //Uma função nque mostra precedência de operadores
 function precedenciaDeOperadores(primeiroParametro, segundoParametro){
-    console.log("Adicionando os parâmetros com precedência temos: " + (primeiroParametro + segundoParametro));
+    console.log("Adicionando os parâmetros com precedência temos: " + (primeiroParametro * segundoParametro));
 }
 
 
 //Agora posso repetir as função quantes vezes quiser no meu código com valores diferentes
 
+//Atribuindo o valor do retorno à uma variável
+let minhaVar = minhaPrimeiraFuncao(2,3);
+console.log(minhaVar);
+console.log(minhaPrimeiraFuncao(minhaVar, minhaVar));
+console.log(minhaVar);
+
+//Simplesmente chamando funções
 minhaSegundaFuncao(2,5);
 minhaSegundaFuncao(4,8);
 precedenciaDeOperadores(7, 11);
--- RecursosJS/Aula_10_Encapsulamento_Classes.js
+++ RecursosJS/Aula_10_Encapsulamento_Classes.js
@@ -1,15 +1,30 @@
-class Pessoa{
-    constructor(nome, sobrenome, idade) {
+class Identidade {
+    constructor(identidade) {
+        this.identidade = identidade;
+    }
+}
+
+class Pessoa {
+
+    constructor(nome, sobrenome, idade, num) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.idade = idade;
+        this.identidade = new Identidade(num);
     }
 
-    apresentando(){
+    apresentando() {
         console.log(`Sou ${this.nome} ${this.sobrenome} e tenho ${this.idade}`);
     }
 
 }
 
-let adriana = new Pessoa("Adriana", "Cerdeira", 20);
+
+let adriana = new Pessoa("Adriana", "Cerdeira", 20, 2222);
 adriana.apresentando();
+console.log(adriana);
+
+let paula = new Pessoa("Paula", "Souza", 35, 4444);
+console.log(paula);
+
+console.log(adriana.identidade.identidade);
