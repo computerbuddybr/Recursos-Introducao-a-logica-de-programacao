0b3f068bc126b05b499e5aa84b12d2065daf13e5
Adriana Cerdeira - Computer Buddy BR <90341758+computerbuddybr@users.noreply.github.com>
2022-02-11 15:32:41 -0300

    Add files via upload

--- RecursosJS/Aula_06_Operadores.js
+++ RecursosJS/Aula_06_Operadores.js
@@ -3,6 +3,7 @@ let b = 2;
 let d = "2";
 
 //Operadores Aritméticos
+console.log("Operadores Aritméticos: ");
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
@@ -12,16 +13,21 @@ console.log(a % b);
 
 
 //Operadores de Incremento e Decremento
+console.log("Operadores Incremento e Decremento: ");
 console.log(a++);
 console.log(a);
 console.log(a--);
 console.log(++a);
 
 //Operadores de Atribuição
+console.log("Operadores de Atribuição: ");
 let c = a + b;
+console.log(c);
 a +=b;
+console.log(a);
 
 //Operadores de Comparação
+console.log("Operadores Comparaçã0: ");
 console.log(a < b);
 console.log(a <= b);
 console.log(b == d);
@@ -30,14 +36,17 @@ console.log(a != b);
 
 
 //Operador Condicional (Ternário)
+console.log("Operadores Ternário: ");
 console.log(a < b ? "Sim" : "Não");
 
 //Operadores lógicos (E, OU, NEGAÇÃO)
+console.log("Operadores Lógicos: ");
 console.log(a > b & b == d);
 console.log(a < b | b == d);
 console.log(!false);
 
 //Precedência de operadores
+console.log("Precedência de Operadores: ");
 console.log(a + b * 2);
 console.log((a + b) * 2);
 console.log(a > b & b == d & b === 2);
